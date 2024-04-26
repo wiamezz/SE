547c0fa68b82b86d8913d446fad1f593d1548842
wiamezz <anonymousw2906@gmail.com>
2024-04-26 21:02:20 +0100

    mysqli -> pdo

--- php/config.php
+++ php/config.php
@@ -1,5 +1,17 @@
-<?php 
- 
- $con = mysqli_connect("localhost","root","","tutorial") or die("Couldn't connect");
+<?php
 
+try {
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "tutorial";
+    $dsn = "mysql:host=localhost;dbname=$db_name";
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+
+    $pdo = new PDO($dsn, $db_username, $db_password, $options);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 ?>
