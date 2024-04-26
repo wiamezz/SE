6293975630413b83b7139884fd1550a5606d83f3
wiamezz <anonymousw2906@gmail.com>
2024-04-26 22:29:26 +0100

    tcp/ip
    Celui-ci se connecte au serveur MySQL en utilisant TCP/IP sur l'adresse de bouclage 127.0.0.1.

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php 
  
- $con = mysqli_connect("localhost","root","","tutorial") or die("Couldn't connect");
+ $conn = mysqli_connect("127.0.0.1", "root", "", "tutorial") or die("Couldn't connect");
 
 ?>
